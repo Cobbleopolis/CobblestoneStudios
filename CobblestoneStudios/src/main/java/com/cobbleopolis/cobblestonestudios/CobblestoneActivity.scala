38368c85d188a72a4fae59de9a9e455f8867a72e
Cobbleopolis <agounimon@yahoo.com>
2015-08-18 00:12:23 -0400

    Played with some dialogs

--- CobblestoneStudios/src/main/java/com/cobbleopolis/cobblestonestudios/CobblestoneActivity.scala
+++ CobblestoneStudios/src/main/java/com/cobbleopolis/cobblestonestudios/CobblestoneActivity.scala
@@ -1,19 +1,44 @@
 package com.cobbleopolis.cobblestonestudios
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
-import android.support.v7.app.{ActionBar, ActionBarActivity}
+import android.support.v7.app.ActionBarActivity
 import android.view.{Menu, MenuItem}
 
 //remove if not needed
 
 class CobblestoneActivity extends ActionBarActivity {
 
+    var logout: AlertDialog.Builder = null
+    var settings: AlertDialog.Builder = null
+
     protected override def onCreate(savedInstanceState: Bundle) {
         super.onCreate(savedInstanceState)
-//        val actionBar: ActionBar = getSupportActionBar
-//        actionBar.setBackgroundDrawable(new ColorDrawable(Color.RED)); // set your desired color
+
+        logout = new AlertDialog.Builder(this)
+        logout.setTitle("Do you want to logout?")
+        logout.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+            def onClick(dialog: DialogInterface, whichButton: Int): Unit = {
+
+            }
+        })
+        logout.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            def onClick(dialog: DialogInterface, whichButton: Int): Unit = {
+            }
+        })
+
+        settings = new AlertDialog.Builder(this)
+        settings.setTitle(R.string.settings_title)
+        settings.setItems(R.array.media_names, new DialogInterface.OnClickListener() {
+            def onClick(dialog: DialogInterface, which: Int): Unit = {
+
+            }
+        })
+
+
         setContentView(R.layout.activity_cobblestone)
     }
 
@@ -25,8 +50,16 @@ class CobblestoneActivity extends ActionBarActivity {
     override def onOptionsItemSelected(item: MenuItem): Boolean = {
         val id = item.getItemId
         if (id == R.id.action_settings) {
+            settings.show()
+            return true
+        } else if (id == R.id.action_logout) {
+            logout.show()
             return true
         }
         super.onOptionsItemSelected(item)
+    }
+
+    override def onBackPressed() {
+        logout.show()
     }
 }
---
+++ CobblestoneStudios/src/main/java/com/cobbleopolis/cobblestonestudios/Dialog.scala
@@ -0,0 +1,8 @@
+package com.cobbleopolis.cobblestonestudios
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+
+object Dialog {
+
+}
